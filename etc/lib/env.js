548b95f6218bb492a2a484b24c43ed1af43655c9
qile222 <chhxxc@gmail.com>
2018-07-02 22:03:22 +0800

    fix test env

--- etc/lib/env.js
+++ etc/lib/env.js
@@ -2,5 +2,5 @@ module.exports = {
   release: 'https://homebase.rokid.com',
   rc: 'https://homebase-pre.rokid.com',
   dev: 'https://homebase.rokid-inc.com',
-  test: 'https://homebase-dev-test.rokid.com/',
+  test: 'https://homebase-dev-test.rokid.com',
 }
